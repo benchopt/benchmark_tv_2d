879f9c729c8ae96d1e8ec9a5872288177aafbaf6
Samuel Vaiter <samuel.vaiter@gmail.com>
2022-05-18 22:26:34 +0200

    Split denoising / deblurring

--- datasets/deblurring.py
+++ datasets/deblurring.py
@@ -8,52 +8,45 @@ with safe_import_context() as import_ctx:
     from scipy.signal.windows import gaussian
 
 
-GAUSSIAN_FILTER = np.outer(
-    gaussian(40, 8),
-    gaussian(40, 8))
-GAUSSIAN_FILTER /= GAUSSIAN_FILTER.sum()
+def make_blur(size, std):
+    gaussian_filter = np.outer(
+        gaussian(size, std),
+        gaussian(size, std))
+    gaussian_filter /= gaussian_filter.sum()
 
+    def blur_2d(u):
+        return fftconvolve(u, gaussian_filter, mode="same")
 
-def blur_2d(u):
-    return fftconvolve(u, GAUSSIAN_FILTER, mode="same")
+    blur_2d.T = blur_2d
+    blur_2d.norm = 1.
 
-
-blur_2d.T = blur_2d
-blur_2d.norm = 1.
-
-
-def identity(u):
-    return u
-
-
-identity.T = identity
-identity.norm = 1.
+    return blur_2d
 
 
 class Dataset(BaseDataset):
 
-    name = "Cartoon"
+    name = "Deblurring"
 
     # List of parameters to generate the datasets. The benchmark will consider
     # the cross product for each key in the dictionary.
     # A * I + bruit ~ N(mu, sigma)
     parameters = {
         'std_noise': [0.3],
-        'scenario': ['denoising', 'deblurring']}
+        'size_blur': [40],
+        'std_blur': [8.],
+    }
 
     def __init__(self, std_noise=0.3,
-                 scenario='denoising', random_state=27):
+                 size_blur=40, std_blur=8.,
+                 random_state=27):
         # Store the parameters of the dataset
         self.std_noise = std_noise
-        self.scenario = scenario
+        self.std_blur = std_blur
         self.random_state = random_state
 
     def set_lin_op(self):
-        if self.scenario == 'deblurring':
-            lin_op = blur_2d
-        else:
-            lin_op = identity
-        return lin_op
+        blur_2d = make_blur(self.size_blur, self.std_blur)
+        return blur_2d
 
     def get_data(self):
         rng = np.random.RandomState(self.random_state)
