4e568a0518bca80a8ac1e2bd152442da60f6537e
Samuel Vaiter <samuel.vaiter@gmail.com>
2022-05-19 09:33:43 +0200

    Fix flake8

--- tests/test_deblurring.py
+++ tests/test_deblurring.py
@@ -1,22 +1,22 @@
 import numpy as np
-import pytest
 
 from datasets.deblurring import make_blur
 
 
 def test_blur_operator_adjoint():
     rng = np.random.RandomState(42)
-    u = rng.normal(0, 1., size=(256,128))
-    v = rng.normal(0, 1., size=(256,128))
+    u = rng.normal(0, 1., size=(256, 128))
+    v = rng.normal(0, 1., size=(256, 128))
     blur = make_blur(27, 3.)
     first_ip = np.sum(np.diag(blur(u).T @ v))
     second_ip = np.sum(np.diag(u.T @ blur(v)))
     np.testing.assert_almost_equal(first_ip, second_ip)
 
+
 def test_blur_operator_norm():
     rng = np.random.RandomState(42)
     blur = make_blur(27, 3.)
-    q = rng.normal(0, 1., size=(256,128))
+    q = rng.normal(0, 1., size=(256, 128))
     # Power method
     for i in range(10000):
         z = blur(q)
