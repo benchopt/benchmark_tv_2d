33ea9fce98ad4e41f553e5259ac215259b16c49f
EnLAI111 <ensophial@gmail.com>
2022-06-13 00:48:58 +0200

    add type_lin_op

--- datasets/simulated.py
+++ datasets/simulated.py
@@ -4,6 +4,23 @@ from benchopt import safe_import_context
 with safe_import_context() as import_ctx:
     import numpy as np
     from scipy.sparse import rand as sprand
+    from scipy.signal import fftconvolve
+    from scipy.signal.windows import gaussian
+
+
+def make_blur(size, std):
+    gaussian_filter = np.outer(
+        gaussian(size, std),
+        gaussian(size, std))
+    gaussian_filter /= gaussian_filter.sum()
+
+    def blur_2d(u):
+        return fftconvolve(u, gaussian_filter, mode="same")
+
+    blur_2d.T = blur_2d
+    blur_2d.norm = 1.
+
+    return blur_2d
 
 
 def identity(u):
@@ -23,16 +40,31 @@ class Dataset(BaseDataset):
     # A * I + bruit ~ N(mu, sigma)
     parameters = {
         'std_noise': [0.3],
-        'subsampling': [4]}
+        'size_blur': [27],
+        'std_blur': [2.],
+        'subsampling': [4],
+        'type_lin_op': ['deblurring', 'denoising'],
+    }
 
-    def __init__(self, std_noise=0.3, subsampling=4, random_state=27):
+    def __init__(self, std_noise=0.3,
+                 size_blur=27, std_blur=8.,
+                 subsampling=4,
+                 random_state=27,
+                 type_lin_op='denoising'):
         # Store the parameters of the dataset
         self.std_noise = std_noise
+        self.size_blur = size_blur
+        self.std_blur = std_blur
         self.subsampling = subsampling
         self.random_state = random_state
+        self.type_lin_op = type_lin_op
 
     def set_lin_op(self):
-        return identity
+        if self.type_lin_op == 'deblurring':
+            blur_2d = make_blur(self.size_blur, self.std_blur)
+            return blur_2d
+        else:
+            return identity
 
     def get_data(self):
         rng = np.random.RandomState(self.random_state)
