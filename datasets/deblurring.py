2bff6cf93b27da8c2234d54c6c65773fb0b798c2
EnLAI111 <ensophial@gmail.com>
2022-06-29 15:00:47 +0200

    add laplace noise

--- datasets/deblurring.py
+++ datasets/deblurring.py
@@ -20,20 +20,22 @@ class Dataset(BaseDataset):
         'std_blur': [2.],
         'subsampling': [4],
         'type_A': ['deblurring'],
+        'type_n': ['gaussian', 'laplace']
     }
 
     def __init__(self, std_noise=0.3,
                  size_blur=27, std_blur=8.,
                  subsampling=4,
                  random_state=27,
-                 type_A='deblurring'):
+                 type_A='deblurring',
+                 type_n='gaussian'):
         # Store the parameters of the dataset
         self.std_noise = std_noise
         self.size_blur = size_blur
         self.std_blur = std_blur
         self.subsampling = subsampling
         self.random_state = random_state
-        self.type_A = type_A
+        self.type_A, self.type_n = type_A, type_n
 
     def set_A(self, height):
         return make_blur(self.type_A, height,
@@ -44,9 +46,14 @@ class Dataset(BaseDataset):
         img = misc.face(gray=True)[::self.subsampling, ::self.subsampling]
         img = img / 255.0
         height, width = img.shape
+        if self.type_n == 'gaussian':
+            # noise ~ N(loc, scale)
+            n = rng.normal(0, self.std_noise, size=(height, width))
+        elif self.type_n == 'laplace':
+            # noise ~ L(loc, scale)
+            n = rng.laplace(0, self.std_noise, size=(height, width))
         A = self.set_A(height)
-        y_degraded = (A @ img +
-                      rng.normal(0, self.std_noise, size=(height, width)))
+        y_degraded = A @ img + n
         data = dict(A=A, y=y_degraded)
 
         return data
