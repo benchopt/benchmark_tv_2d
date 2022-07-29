168cd0c9f733eb565ec4aebbafac431abdd8899f
EnLAI111 <ensophial@gmail.com>
2022-07-29 16:42:41 +0200

    fix

--- solvers/DPGD.py
+++ solvers/DPGD.py
@@ -6,8 +6,8 @@ with safe_import_context() as import_ctx:
     import numpy as np
     from scipy.sparse.linalg import LinearOperator
     from scipy.sparse.linalg import cg
-    div = import_ctx.import_from('matrix_op', 'div')
-    grad = import_ctx.import_from('matrix_op', 'grad')
+    div = import_ctx.import_from('matrice_op', 'div')
+    grad = import_ctx.import_from('matrice_op', 'grad')
     dual_prox_tv_aniso = import_ctx.import_from('matrice_op',
                                                 'dual_prox_tv_aniso')
     dual_prox_tv_iso = import_ctx.import_from('matrice_op', 'dual_prox_tv_iso')
