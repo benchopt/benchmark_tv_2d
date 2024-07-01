2a8063197c86e755b6611dbc84c97f7f08c70203
wassimmazouz <wassimmazouz5261@gmail.com>
2024-07-01 15:14:52 +0200

    Added DRUNet reconstruction

--- solvers/deepinv.py
+++ solvers/deepinv.py
@@ -12,7 +12,7 @@ class Solver(BaseSolver):
     parameters = {
         'reg': [0.1],
         'gamma': [1],
-        'max_iter': [50]
+        'recon': ['TV', 'DRUNet']
     }
 
     def skip(self, A, reg, delta, data_fit, y, isotropy):
@@ -36,7 +36,13 @@ class Solver(BaseSolver):
         x = y.clone().to(device)
         x = x.unsqueeze(0)
         data_fidelity = L2()
-        prior = dinv.optim.TVPrior()
+        if self.recon == 'TV':
+            prior = dinv.optim.TVPrior()
+        elif self.recon == 'DRUNet':
+            denoiser = dinv.models.DRUNet(
+                in_channels=1, out_channels=1, pretrained='download',
+                device=device)
+            prior = dinv.optim.PnP(denoiser=denoiser)
 
         physics = dinv.physics.Inpainting(
             tensor_size=x.shape,
@@ -44,9 +50,12 @@ class Solver(BaseSolver):
             device=device
         )
         physics.noise_model = dinv.physics.GaussianNoise(sigma=0.2)
-        for _ in range(50):
+        for _ in range(n_iter):
             x = x - gamma*data_fidelity.grad(x, y, physics)
-            x = prior.prox(x,  gamma=gamma*reg)
+            if self.recon == 'TV':
+                x = prior.prox(x,  gamma=gamma*reg)
+            elif self.recon == 'DRUNet':
+                x = denoiser(x, gamma*reg)
         self.out = x.clone()
         self.out = self.out.squeeze(0)
         self.out = self.out.squeeze(0)
